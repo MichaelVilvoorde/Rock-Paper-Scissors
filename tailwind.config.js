1bcdcca482a0fcd210f71a16372b3246e2235276
MichaelVilvoorde <60927311+MichaelVilvoorde@users.noreply.github.com>
2021-03-05 15:51:50 +0100

    Basic layout set up

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#F5A011',
+        'secondary':'#EAEAEA',
+        'black-2':'#100F24',
+      },
+    },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
