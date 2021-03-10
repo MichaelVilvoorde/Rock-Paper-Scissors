170f82940a30a91a1a1049b1ba625298f2c19bff
MichaelVilvoorde <60927311+MichaelVilvoorde@users.noreply.github.com>
2021-03-10 12:27:03 +0100

    finalize simplify code

--- dist/main.js
+++ dist/main.js
@@ -1,100 +1,62 @@
 document.getElementById("playAgainButton").style.visibility = "hidden";
 
-
-
-function computerChoice() {
+function singleRound(clicked_id){
     const choices = [
         ["rock"],
         ["paper"],
         ["scissors"] 
-      ];
-    
-      compChoice = choices[Math.floor(Math.random() * choices.length)];
-        console.log(compChoice);
+    ];
 
-    return compChoice;
-
-}
- 
-
-
-function rock() {
-         
-    document.getElementById("result").innerHTML = "You picked: rock." + " " + "The AI picked: " + compChoice;
+    compChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(compChoice);
 
     humanScore = document.getElementById("humanScore").innerHTML;
     computerScore = document.getElementById("computerScore").innerHTML;
 
+    document.getElementById("result").innerHTML = "You picked: " + clicked_id + ". " + "The AI picked: " + compChoice + ".";
 
-
-    if (compChoice == "rock") {
+    // player choise rock
+    if (clicked_id == "rock" && compChoice == "rock") {
         document.getElementById("outcome").innerHTML = "It's a draw!";
     }
-    if (compChoice == "paper") {
+
+    if (clicked_id == "rock" && compChoice == "paper") {
         document.getElementById("outcome").innerHTML = "paper beats rock -> You lose!";
         document.getElementById("computerScore").innerHTML = parseInt(computerScore) + 1;
     }
-    if (compChoice == "scissors") {
+
+    if (clicked_id == "rock" && compChoice == "scissors") {
         document.getElementById("outcome").innerHTML = "scissors beats rock -> You win!";
         document.getElementById("humanScore").innerHTML = parseInt(humanScore) + 1;
     }
 
-   
-}
-
-function paper() {
-    
-    
-   
-    document.getElementById("result").innerHTML = "You picked: paper." + " " + "The AI picked: " + compChoice;
-
-
-    humanScore = document.getElementById("humanScore").innerHTML;
-    computerScore = document.getElementById("computerScore").innerHTML;
-
-    if (compChoice == "rock") {
+    // player choise paper
+    if (clicked_id == "paper" && compChoice == "rock") {
         document.getElementById("outcome").innerHTML = "paper beats rock -> You win!";
         document.getElementById("humanScore").innerHTML = parseInt(humanScore) + 1;
     }
-    if (compChoice == "paper") {
+    if (clicked_id == "paper" && compChoice == "paper") {
         document.getElementById("outcome").innerHTML = "It's a draw!";
     }
-    if (compChoice == "scissors") {
+    if (clicked_id == "paper" && compChoice == "scissors") {
         document.getElementById("outcome").innerHTML = "scissors beats paper -> You lose!";
         document.getElementById("computerScore").innerHTML = parseInt(computerScore) + 1;
     }
-    
-
-    
-}
-
-function scissors() {
-
-    
-
-    document.getElementById("result").innerHTML = "You picked: scissors." + " " + "The AI picked: " + compChoice;   
-
-    humanScore = document.getElementById("humanScore").innerHTML;
-    computerScore = document.getElementById("computerScore").innerHTML;
 
 
-    if (compChoice == "rock") {
+    // player choise scissors
+    if (clicked_id == "scissors" && compChoice == "rock") {
         document.getElementById("outcome").innerHTML = "rock beats scissors -> You lose!";
         document.getElementById("computerScore").innerHTML = parseInt(computerScore) + 1;
     }
-    if (compChoice == "paper") {
+    if (clicked_id == "scissors" && compChoice == "paper") {
         document.getElementById("outcome").innerHTML = "sciccors beats paper -> You win!";
         document.getElementById("humanScore").innerHTML = parseInt(humanScore) + 1;
     }
-    if (compChoice == "scissors") {
+    if (clicked_id == "scissors" && compChoice == "scissors") {
         document.getElementById("outcome").innerHTML = "It's a draw!";
-
     }
-    
-
-
 }
-
 
 
 function maxScoreCheck(){
@@ -102,9 +64,9 @@ function maxScoreCheck(){
     computerScore = document.getElementById("computerScore").innerHTML;
 
     if (computerScore == 5 || humanScore == 5) { 
-        document.getElementById("playButton1").disabled = true;
-        document.getElementById("playButton2").disabled = true;
-        document.getElementById("playButton3").disabled = true;
+        document.getElementById("rock").disabled = true;
+        document.getElementById("paper").disabled = true;
+        document.getElementById("scissors").disabled = true;
         document.getElementById("playAgainButton").style.visibility = "visible";
 
         if (computerScore == 5) {
@@ -115,23 +77,16 @@ function maxScoreCheck(){
             document.getElementById("finalResult").innerHTML = "Congratulations, you win the game :)";
         }
     }
-    
-    
-    
-    console.log(humanScore);
-    console.log(computerScore);
-
 }
 
 
 function playAgain() {
     document.getElementById("humanScore").innerHTML = 0;
     document.getElementById("computerScore").innerHTML = 0;
-    document.getElementById("playButton1").disabled = false;
-    document.getElementById("playButton2").disabled = false;
-    document.getElementById("playButton3").disabled = false;
-    
-
+    document.getElementById("rock").disabled = false;
+    document.getElementById("paper").disabled = false;
+    document.getElementById("scissors").disabled = false;
+    document.getElementById("finalResult").innerHTML = "Take your pick!";
 }
 
 
