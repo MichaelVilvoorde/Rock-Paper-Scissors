8f181aad5aa10cf02bfc7ecb0285de040aea7cc3
MichaelVilvoorde <60927311+MichaelVilvoorde@users.noreply.github.com>
2021-03-10 11:01:12 +0100

    simplify JS

--- dist/main.js
+++ dist/main.js
@@ -1,32 +1,40 @@
+document.getElementById("playAgainButton").style.visibility = "hidden";
 
-const choices = [
-    ["rock"],
-    ["paper"],
-    ["scissors"] 
-  ];
 
-  document.getElementById("playAgainButton").style.visibility = "hidden";
+
+function computerChoice() {
+    const choices = [
+        ["rock"],
+        ["paper"],
+        ["scissors"] 
+      ];
+    
+      compChoice = choices[Math.floor(Math.random() * choices.length)];
+        console.log(compChoice);
+
+    return compChoice;
+
+}
+ 
 
 
 function rock() {
-    
-    CompChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(CompChoice);
-
-    document.getElementById("result").innerHTML = "You picked: rock." + " " + "The AI picked: " + CompChoice;
+         
+    document.getElementById("result").innerHTML = "You picked: rock." + " " + "The AI picked: " + compChoice;
 
     humanScore = document.getElementById("humanScore").innerHTML;
     computerScore = document.getElementById("computerScore").innerHTML;
 
 
-    if (CompChoice == "rock") {
+
+    if (compChoice == "rock") {
         document.getElementById("outcome").innerHTML = "It's a draw!";
     }
-    if (CompChoice == "paper") {
+    if (compChoice == "paper") {
         document.getElementById("outcome").innerHTML = "paper beats rock -> You lose!";
         document.getElementById("computerScore").innerHTML = parseInt(computerScore) + 1;
     }
-    if (CompChoice == "scissors") {
+    if (compChoice == "scissors") {
         document.getElementById("outcome").innerHTML = "scissors beats rock -> You win!";
         document.getElementById("humanScore").innerHTML = parseInt(humanScore) + 1;
     }
@@ -36,23 +44,22 @@ function rock() {
 
 function paper() {
     
-    CompChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(CompChoice);
+    
    
-    document.getElementById("result").innerHTML = "You picked: paper." + " " + "The AI picked: " + CompChoice;
+    document.getElementById("result").innerHTML = "You picked: paper." + " " + "The AI picked: " + compChoice;
+
 
     humanScore = document.getElementById("humanScore").innerHTML;
     computerScore = document.getElementById("computerScore").innerHTML;
 
-
-    if (CompChoice == "rock") {
+    if (compChoice == "rock") {
         document.getElementById("outcome").innerHTML = "paper beats rock -> You win!";
         document.getElementById("humanScore").innerHTML = parseInt(humanScore) + 1;
     }
-    if (CompChoice == "paper") {
+    if (compChoice == "paper") {
         document.getElementById("outcome").innerHTML = "It's a draw!";
     }
-    if (CompChoice == "scissors") {
+    if (compChoice == "scissors") {
         document.getElementById("outcome").innerHTML = "scissors beats paper -> You lose!";
         document.getElementById("computerScore").innerHTML = parseInt(computerScore) + 1;
     }
@@ -63,24 +70,23 @@ function paper() {
 
 function scissors() {
 
-    CompChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(CompChoice);
-
-    document.getElementById("result").innerHTML = "You picked: scissors." + " " + "The AI picked: " + CompChoice;   
     
+
+    document.getElementById("result").innerHTML = "You picked: scissors." + " " + "The AI picked: " + compChoice;   
+
     humanScore = document.getElementById("humanScore").innerHTML;
     computerScore = document.getElementById("computerScore").innerHTML;
 
 
-    if (CompChoice == "rock") {
+    if (compChoice == "rock") {
         document.getElementById("outcome").innerHTML = "rock beats scissors -> You lose!";
         document.getElementById("computerScore").innerHTML = parseInt(computerScore) + 1;
     }
-    if (CompChoice == "paper") {
+    if (compChoice == "paper") {
         document.getElementById("outcome").innerHTML = "sciccors beats paper -> You win!";
         document.getElementById("humanScore").innerHTML = parseInt(humanScore) + 1;
     }
-    if (CompChoice == "scissors") {
+    if (compChoice == "scissors") {
         document.getElementById("outcome").innerHTML = "It's a draw!";
 
     }
@@ -89,27 +95,28 @@ function scissors() {
 
 }
 
+
+
 function maxScoreCheck(){
     humanScore = document.getElementById("humanScore").innerHTML;
     computerScore = document.getElementById("computerScore").innerHTML;
-    
-    if (computerScore == 5) {
-        document.getElementById("finalResult").innerHTML = "Sorry, you losed the game :(";
-        document.getElementById("playButton").disabled = true;
-        document.getElementById("playButton2").disabled = true;
-        document.getElementById("playButton3").disabled = true;
-        document.getElementById("playAgainButton").style.visibility = "visible";
-    }
-    
-    if (humanScore == 5) {
-        document.getElementById("finalResult").innerHTML = "Congratulations, you win the game :)";
-        document.getElementById("playButton").disabled = true;
-        document.getElementById("playButton2").disabled = true;
-        document.getElementById("playButton3").disabled = true;
-        document.getElementById("playAgainButton").style.visibility = "visible";
-        
-    }
 
+    if (computerScore == 5 || humanScore == 5) { 
+        document.getElementById("playButton1").disabled = true;
+        document.getElementById("playButton2").disabled = true;
+        document.getElementById("playButton3").disabled = true;
+        document.getElementById("playAgainButton").style.visibility = "visible";
+
+        if (computerScore == 5) {
+            document.getElementById("finalResult").innerHTML = "Sorry, you losed the game :(";
+        }
+        
+        if (humanScore == 5) {
+            document.getElementById("finalResult").innerHTML = "Congratulations, you win the game :)";
+        }
+    }
+    
+    
     
     console.log(humanScore);
     console.log(computerScore);
@@ -120,7 +127,7 @@ function maxScoreCheck(){
 function playAgain() {
     document.getElementById("humanScore").innerHTML = 0;
     document.getElementById("computerScore").innerHTML = 0;
-    document.getElementById("playButton").disabled = false;
+    document.getElementById("playButton1").disabled = false;
     document.getElementById("playButton2").disabled = false;
     document.getElementById("playButton3").disabled = false;
     
