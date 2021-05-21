5a2ba44f0ede3d574527367f86e7d58a82663bc5
MichaelVilvoorde <60927311+MichaelVilvoorde@users.noreply.github.com>
2021-05-21 16:09:46 +0200

    primary color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary':'#F77500',
+        'primary':'#EF8026',
         'secondary':'#EAEAEA',
         'black-2':'#100F24',
       },
